8bc683e15d7738159d0727edfa34a5c1b15a3738
Negin Alphi <negin.alipanahi@gmail.com>
2024-01-14 16:03:03 +0330

    Builing Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,16 +2,16 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
 const useGames = () => {
-  interface Game {
-    id: number;
-    name: string;
-  }
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
