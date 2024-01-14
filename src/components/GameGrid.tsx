6b85edccc24f02b551bc0699f8e04dff306b9fd2
Negin Alphi <negin.alipanahi@gmail.com>
2024-01-15 00:44:01 +0330

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,16 +4,31 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+interface Props {
+  selectedGenre: Genre | null
+} 
+
+const GameGrid = ({selectedGenre}: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm:1, md:2, lg:3, xl:5 }} padding={10} spacing={3}>
-        {isLoading && skeletons.map(skeleton => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer>)}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={10}
+        spacing={3}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
         {data.map((game) => (
-         <GameCardContainer><GameCard key={game.id} game={game} /></GameCardContainer> 
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -33,7 +33,7 @@ const PlatformIconList = ({ plarforms }: Props) => {
   return (
     <HStack marginY={1}>
       {plarforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color='gray.500' />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500' />
       ))}
     </HStack>
   );
