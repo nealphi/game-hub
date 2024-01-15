d71331392b10c335c2407fd4044a4ffdb136eaa8
Negin Alphi <negin.alipanahi@gmail.com>
2024-01-15 17:09:45 +0330

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -20,11 +20,11 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' scrollMarginBottom={3}>
         <PlatformIconList plarforms={game.parent_platforms.map( p => p.platform)} />
         <CreditScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
       </CardBody>
     </Card>
   );
